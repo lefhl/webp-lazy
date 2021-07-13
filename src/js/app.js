dc7e27c0c3c60fa01312bb14a94f1b874a1b567b
e.gizbrekht <gizbreht.e@yandex.ru>
2021-07-13 13:58:21 +0300

    add lozad

--- src/js/app.js
+++ src/js/app.js
@@ -1,5 +1,6 @@
 import "@fancyapps/fancybox";
 import "./foundation.js";
+import lozad from "lozad";
 import "./fontAwesome.js";
 import "./chooseCity.js";
 import "./carousels.js";
@@ -13,6 +14,12 @@ import "../scss/style.scss";
 (function ($) {
   "use strict";
   $(function () {
-    // your code here
+    lozad(".lozad", {
+      rootMargin: "40px 0px",
+      loaded: function (el) {
+        el.classList.add("loaded");
+      },
+    }).observe();
+
   });
 })(jQuery);
